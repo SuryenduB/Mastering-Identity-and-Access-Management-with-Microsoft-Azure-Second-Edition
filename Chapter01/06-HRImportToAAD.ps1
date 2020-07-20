3209a76c0d7403fbcf3d75449f28b2f2237114c9
SuryenduB <53970796+SuryenduB@users.noreply.github.com>
2020-07-20 19:19:16 +0530

    Update 06-HRImportToAAD.ps1

--- Chapter01/06-HRImportToAAD.ps1
+++ Chapter01/06-HRImportToAAD.ps1
@@ -1,27 +1,47 @@
 #Implement log function
 function logit($level, $message)
-{
-	switch($level)
+
+{
+
+
+	switch($level)
+
 	{
-	        1 {$prefix = "INFO: "}        
-		2 {$prefix = "WARNING: "}        
-		3 {$prefix = "ERROR: "}        
-		default {$prefix = "UNKNOWN"}
-	}    write-host $prefix$message
-}
+	
+        1 {$prefix = "INFO: "}
+        
+		2 {$prefix = "WARNING: "}
+        
+		3 {$prefix = "ERROR: "}
+        
+		default {$prefix = "UNKNOWN"}
+
+	}
+
+    write-host $prefix$message
+
+}
+
 
 #Connect to the Microsoft Online Service
-Logit 1 "Getting Microsoft Azure Active Directory Credentials...."
+
+Logit 1 "Getting Microsoft Azure Active Directory Credentials...."
+
+
 Connect-MsolService
 
 #Set domain variable
 Logit 1 "Gettting Domain Variable..."
-$domain = Get-MsolDomain | where {$_.Name -notlike "*mail*"}
+$domain = Get-MsolDomain 
 
 #Set configuration directory location
 Logit 1 "Settting path to HR export file..."
-$dir = "C:\Configuration\HRExports"
+$dir = "C:\Users\sb277\Documents\Mastering-Identity-and-Access-Management-with-Microsoft-Azure-Second-Edition-master\Chapter01"
 
 #CSV HR export file to import in Azure Active Directory
 Logit 1 "Import Users and assign License...."
-(get-content "$($dir)\NewHire.csv") | foreach-object {$_ -replace "contoso.com" , $domain.Name} | Set-Content "$($dir)\NewHire.csv"import-csv "$($dir)\NewHire.csv" | foreach {New-Msoluser -userPrincipalName $_.UserPrincipalName -displayname $_.displayname -firstname $_.firstname -lastname $_.lastname -password $_.Password -mobilephone $phone -forcechangepassword $false -usagelocation "CH" | set-msoluserlicense -addlicenses "$($domain.name -replace ".onmicrosoft.com" ,":EMSPREMIUM")","$($domain.name -replace ".onmicrosoft.com" , ":ENTERPRISEPREMIUM")"}
+(get-content "$($dir)\07-NewHire.csv") | foreach-object {$_ -replace "suryendub.onmicrosoft.com" , $domain.Name} | Set-Content "$($dir)\07-NewHire.csv"
+import-csv "$($dir)\07-NewHire.csv" | foreach {
+Get-Msoluser -userPrincipalName $_.UserPrincipalName  | set-msoluserlicense -addlicenses "$($domain.name -replace ".onmicrosoft.com" ,":EMSPREMIUM")"
+}
+(import-csv "$($dir)\07-NewHire.csv") | foreach {New-Msoluser -userPrincipalName $_.UserPrincipalName -displayname $_.displayname -firstname $_.firstname -lastname $_.lastname -password $_.Password -mobilephone $phone -forcechangepassword $false -usagelocation "CH" | set-msoluserlicense -addlicenses "$($domain.name -replace ".onmicrosoft.com" ,":EMSPREMIUM")","$($domain.name -replace ".onmicrosoft.com" , ":EMSPREMIUM")"}
